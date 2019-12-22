03fe7a9ed51d60ca2085cbeb3bbf113ef3373088
Toshiki Asamoto <asamottoo.works@gmail.com>
2019-12-22 21:13:38 +0900

    テストを実装した。

--- src/main/java/org/pickles/oopexcercise001/ReiwaHoliday.java
+++ src/main/java/org/pickles/oopexcercise001/ReiwaHoliday.java
@@ -23,6 +23,9 @@ public class ReiwaHoliday {
 		} else if (Integer.parseInt(in.substring(0, 4)) < 2019) {
 			throw new IllegalArgumentException();
 
+		} else if (!in.substring(4, 5).equals("/") || !in.substring(7, 8).equals("/")) {
+			throw new IllegalArgumentException();
+
 		} else if (Integer.parseInt(in.substring(5, 7)) < 1) {
 			throw new IllegalArgumentException();
 
@@ -39,8 +42,72 @@ public class ReiwaHoliday {
 		return false;
 	}
 
+	private String dateString(int[] targetDate) {
+		String year = String.valueOf(targetDate[0]);
+		String month = (targetDate[1] < 10 ? "0" : "") + String.valueOf(targetDate[1]);
+		String day = (targetDate[2] < 10 ? "0" : "") + String.valueOf(targetDate[2]);
+		return year + "/" + month + "/" + day;
+	}
+
 	private boolean isAdditionalHoliday(String in) {
+		int[] targetDate = new int[3];
+		targetDate[0] = Integer.parseInt(in.substring(0, 4));
+		targetDate[1] = Integer.parseInt(in.substring(5, 7));
+		targetDate[2] = Integer.parseInt(in.substring(8, 10));
+
+		int[] yesterday = getYesterday(targetDate);
+		String yesterdayString = dateString(yesterday);
+
+		int[] tomorrow = getTomorrow(targetDate);
+		String tomorrowString = dateString(tomorrow);
+
+		if (targetDate[1] == 5 && targetDate[2] == 6 && getDayOfWeekNumber(targetDate[0], targetDate[1], targetDate[2]) == 3) {
+			return true;
+		} else if (isMonday(targetDate[0], targetDate[1], targetDate[2]) && isPublicHoliday(yesterdayString)) {
+			return true;
+		} else if (isPublicHoliday(yesterdayString) && isPublicHoliday(tomorrowString)) {
+			return true;
+		}
+
 		return false;
+	}
+
+	private int[] getYesterday(int[] targetDate) {
+		int[] res = new int[3];
+		res[0] = targetDate[0];
+		res[1] = targetDate[1];
+		res[2] = targetDate[2];
+		res[2] -= 1;
+		if (res[2] < 1) {
+			res[1] -= 1;
+			if (res[1] < 1) {
+				res[0] -= 1;
+				res[1] = 12;
+			}
+			int[] daysOfMonth = getDaysOfMonth(res[0]);
+			res[2] = daysOfMonth[res[1]];
+		}
+		return res;
+	}
+
+	private int[] getTomorrow(int[] targetDate) {
+		int[] res = new int[3];
+		res[0] = targetDate[0];
+		res[1] = targetDate[1];
+		res[2] = targetDate[2];
+
+		int[] targetDaysOfMonth = getDaysOfMonth(targetDate[0]);
+
+		res[2] += 1;
+		if (res[2] > targetDaysOfMonth[targetDate[1]]) {
+			res[1] += 1;
+			if (res[1] > 12) {
+				res[0] += 1;
+				res[1] = 1;
+			}
+			res[2] = 1;
+		}
+		return res;
 	}
 
 	private boolean isPublicHoliday(String in) {
@@ -54,7 +121,7 @@ public class ReiwaHoliday {
 			} else {
 				if (Integer.parseInt(in.substring(8, 10)) == 1) {
 					return true;
-				} else if (Integer.parseInt(in.substring(8, 10)) / 7 == 1 &&
+				} else if ((Integer.parseInt(in.substring(8, 10)) - 1) / 7 == 1 &&
 						isMonday(Integer.parseInt(in.substring(0, 4)),
 								Integer.parseInt(in.substring(5, 7)),
 								Integer.parseInt(in.substring(8, 10)))) {
@@ -72,6 +139,9 @@ public class ReiwaHoliday {
 
 			} else {
 				if (Integer.parseInt(in.substring(8, 10)) == 11) {
+					return true;
+				}
+				if (Integer.parseInt(in.substring(8, 10)) == 23) {
 					return true;
 				}
 			}
@@ -136,10 +206,12 @@ public class ReiwaHoliday {
 		} else if (Integer.parseInt(in.substring(5, 7)) == 7) {
 			if (Integer.parseInt(in.substring(8, 10)) < 1 || Integer.parseInt(in.substring(8, 10)) > 31) {
 				throw new IllegalArgumentException();
-			} else if (Integer.parseInt(in.substring(8, 10)) / 7 == 2 &&
+			} else if (Integer.parseInt(in.substring(0, 4)) != 2020 && (Integer.parseInt(in.substring(8, 10)) - 1) / 7 == 2 &&
 					isMonday(Integer.parseInt(in.substring(0, 4)),
 							Integer.parseInt(in.substring(5, 7)),
 							Integer.parseInt(in.substring(8, 10)))) {
+				return true;
+			} else if (Integer.parseInt(in.substring(0, 4)) == 2020 && (Integer.parseInt(in.substring(8, 10)) == 23 || Integer.parseInt(in.substring(8, 10)) == 24)) {
 				return true;
 			}
 
@@ -147,7 +219,9 @@ public class ReiwaHoliday {
 			if (Integer.parseInt(in.substring(8, 10)) < 1 || Integer.parseInt(in.substring(8, 10)) > 31) {
 				throw new IllegalArgumentException();
 			} else {
-				if (Integer.parseInt(in.substring(8, 10)) == 11) {
+				if (Integer.parseInt(in.substring(0, 4)) == 2020 && Integer.parseInt(in.substring(8, 10)) == 10) {
+					return true;
+				} else if (Integer.parseInt(in.substring(0, 4)) != 2020 && Integer.parseInt(in.substring(8, 10)) == 11) {
 					return true;
 				}
 			}
@@ -156,7 +230,7 @@ public class ReiwaHoliday {
 			if (Integer.parseInt(in.substring(8, 10)) < 1 || Integer.parseInt(in.substring(8, 10)) > 30) {
 				throw new IllegalArgumentException();
 			} else {
-				if (Integer.parseInt(in.substring(8, 10)) / 7 == 2 &&
+				if ((Integer.parseInt(in.substring(8, 10)) - 1) / 7 == 2 &&
 						isMonday(Integer.parseInt(in.substring(0, 4)),
 								Integer.parseInt(in.substring(5, 7)),
 								Integer.parseInt(in.substring(8, 10)))) {
@@ -170,10 +244,12 @@ public class ReiwaHoliday {
 			if (Integer.parseInt(in.substring(8, 10)) < 1 || Integer.parseInt(in.substring(8, 10)) > 31) {
 				throw new IllegalArgumentException();
 			} else {
-				if (Integer.parseInt(in.substring(8, 10)) / 7 == 1 &&
+				if (Integer.parseInt(in.substring(0, 4)) != 2020 && (Integer.parseInt(in.substring(8, 10)) - 1) / 7 == 1 &&
 						isMonday(Integer.parseInt(in.substring(0, 4)),
 								Integer.parseInt(in.substring(5, 7)),
 								Integer.parseInt(in.substring(8, 10)))) {
+					return true;
+				} else if (Integer.parseInt(in.substring(0, 4)) == 2019 && Integer.parseInt(in.substring(8, 10)) == 22) {
 					return true;
 				}
 			}
@@ -190,7 +266,7 @@ public class ReiwaHoliday {
 			}
 
 		} else if (Integer.parseInt(in.substring(5, 7)) == 12) {
-			if (Integer.parseInt(in.substring(8, 10)) < 1 || Integer.parseInt(in.substring(8, 10)) > 30) {
+			if (Integer.parseInt(in.substring(8, 10)) < 1 || Integer.parseInt(in.substring(8, 10)) > 31) {
 				throw new IllegalArgumentException();
 			} else {
 				return false;
@@ -200,15 +276,15 @@ public class ReiwaHoliday {
 	}
 
 	private boolean isNormalHoliday(int year, int month, int day) {
-		int dayOfWeek = dayOfWeekNumber(year, month, day);
+		int dayOfWeek = getDayOfWeekNumber(year, month, day);
 		return dayOfWeek == 0 || dayOfWeek == 6;
 	}
 
 	private boolean isMonday(int year, int month, int day) {
-		return dayOfWeekNumber(year, month, day) == 1;
+		return getDayOfWeekNumber(year, month, day) == 1;
 	}
 
-	private int dayOfWeekNumber(int year, int month, int day) {
+	private int getDayOfWeekNumber(int year, int month, int day) {
 		// 日:0, 月:1, 火:2, 水:3, 木:4, 金:5, 土:6
 		int dayOfWeek = 2; // 2019/01/01は火曜日
 		for (int baseYear = 2019; baseYear < year; baseYear++) {
--- src/test/java/org/pickles/oopexcercise001/ReiwaHolidayTest.java
+++ src/test/java/org/pickles/oopexcercise001/ReiwaHolidayTest.java
@@ -2,11 +2,132 @@ package org.pickles.oopexcercise001;
 
 import org.junit.Test;
 
+import java.util.function.Supplier;
+
 import static org.junit.Assert.*;
 
 public class ReiwaHolidayTest {
 
+	private static <T> boolean isCatchException(Supplier<T> supplier) {
+		try {
+			supplier.get();
+			return false;
+		} catch (Exception e) {
+			return true;
+		}
+	}
+
 	@Test
-	public void check() {
+	public void checkIllegalArgumentException() {
+		ReiwaHoliday checker = new ReiwaHoliday();
+		assertTrue(isCatchException(() -> checker.check(null)));
+		assertTrue(isCatchException(() -> checker.check("")));
+		assertTrue(isCatchException(() -> checker.check("19/05/01")));
+		assertTrue(isCatchException(() -> checker.check("2019-05-01")));
+		assertTrue(isCatchException(() -> checker.check("2019/04/30")));
+		assertTrue(isCatchException(() -> checker.check("2019/05/32")));
+		assertTrue(isCatchException(() -> checker.check("2019/05/00")));
+		assertTrue(isCatchException(() -> checker.check("2019/05/-1")));
+	}
+
+	@Test
+	public void checkPublicHoliday2019() {
+		ReiwaHoliday checker = new ReiwaHoliday();
+		assertTrue(checker.check("2019/05/01"));
+		assertTrue(checker.check("2019/05/03"));
+		assertTrue(checker.check("2019/05/04"));
+		assertTrue(checker.check("2019/05/05"));
+		assertTrue(checker.check("2019/07/15"));
+		assertTrue(checker.check("2019/08/11"));
+		assertTrue(checker.check("2019/09/16"));
+		assertTrue(checker.check("2019/09/23"));
+		assertTrue(checker.check("2019/10/14"));
+		assertTrue(checker.check("2019/10/22"));
+		assertTrue(checker.check("2019/11/03"));
+		assertTrue(checker.check("2019/11/23"));
+	}
+
+	@Test
+	public void checkPublicHoliday2020() {
+		ReiwaHoliday checker = new ReiwaHoliday();
+		assertTrue(checker.check("2020/01/01"));
+		assertTrue(checker.check("2020/01/13"));
+		assertTrue(checker.check("2020/02/11"));
+		assertTrue(checker.check("2020/02/23"));
+		assertTrue(checker.check("2020/03/21"));
+		assertTrue(checker.check("2020/04/29"));
+		assertTrue(checker.check("2020/05/03"));
+		assertTrue(checker.check("2020/05/04"));
+		assertTrue(checker.check("2020/05/05"));
+		assertTrue(checker.check("2020/07/23"));
+		assertTrue(checker.check("2020/07/24"));
+		assertTrue(checker.check("2020/08/10"));
+		assertTrue(checker.check("2020/09/21"));
+		assertTrue(checker.check("2020/09/23"));
+		assertTrue(checker.check("2020/11/03"));
+		assertTrue(checker.check("2020/11/23"));
+	}
+
+	@Test
+	public void checkPublicHoliday2021() {
+		ReiwaHoliday checker = new ReiwaHoliday();
+		assertTrue(checker.check("2021/01/01"));
+		assertTrue(checker.check("2021/01/11"));
+		assertTrue(checker.check("2021/02/11"));
+		assertTrue(checker.check("2021/02/23"));
+		assertTrue(checker.check("2021/03/21"));
+		assertTrue(checker.check("2021/04/29"));
+		assertTrue(checker.check("2021/05/01"));
+		assertTrue(checker.check("2021/05/03"));
+		assertTrue(checker.check("2021/05/04"));
+		assertTrue(checker.check("2021/05/05"));
+		assertTrue(checker.check("2021/07/19"));
+		assertTrue(checker.check("2021/08/11"));
+		assertTrue(checker.check("2021/09/20"));
+		assertTrue(checker.check("2021/09/23"));
+		assertTrue(checker.check("2021/10/11"));
+		assertTrue(checker.check("2021/11/03"));
+		assertTrue(checker.check("2021/11/23"));
+	}
+
+	@Test
+	public void checkWeekdayBeforePublicHoliday2019() {
+		ReiwaHoliday checker = new ReiwaHoliday();
+		assertFalse(checker.check("2019/10/21"));
+		assertFalse(checker.check("2019/11/22"));
+	}
+
+	@Test
+	public void checkWeekdayAfterPublicHoliday2019() {
+		ReiwaHoliday checker = new ReiwaHoliday();
+		assertFalse(checker.check("2019/07/16"));
+		assertFalse(checker.check("2019/09/17"));
+		assertFalse(checker.check("2019/09/24"));
+		assertFalse(checker.check("2019/10/15"));
+		assertFalse(checker.check("2019/10/23"));
+	}
+
+	@Test
+	public void checkWeekdayBeforePublicHoliday2020() {
+		ReiwaHoliday checker = new ReiwaHoliday();
+		assertFalse(checker.check("2019/12/31"));
+		assertFalse(checker.check("2020/02/10"));
+		assertFalse(checker.check("2020/03/20"));
+		assertFalse(checker.check("2020/04/28"));
+		assertFalse(checker.check("2020/07/22"));
+		assertFalse(checker.check("2020/11/02"));
+	}
+
+	@Test
+	public void checkWeekdayAfterPublicHoliday2020() {
+		ReiwaHoliday checker = new ReiwaHoliday();
+		assertFalse(checker.check("2020/01/02"));
+		assertFalse(checker.check("2020/01/14"));
+		assertFalse(checker.check("2020/02/12"));
+		assertFalse(checker.check("2020/04/30"));
+		assertFalse(checker.check("2020/08/11"));
+		assertFalse(checker.check("2020/09/24"));
+		assertFalse(checker.check("2020/11/04"));
+		assertFalse(checker.check("2020/11/24"));
 	}
 }
